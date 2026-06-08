7e9aa43c098d7914d7008da01e84c6d5df9a11ef
Shubham25dec <shubhamsingh25sn@gmail.com>
2026-06-08 10:27:52 +0530

    remove includes and func definitions from main (which were accidently added)

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -3,35 +3,36 @@
 #include <SFML/Main.hpp>
 #include <SFML/Audio.hpp>
 
-
 #include "helper.hpp"//namespace qq
 #include "utils.hpp"
 #include "constants.hpp"
 #include "game.hpp"
 #include "animation.hpp"
 
+//++++++++++++++++++++++++++++++++++++
+#if defined(__ANDROID__)
+#include <android/native_activity.h>
+#include <SFML/System/NativeActivity.hpp>
+
+std::string getAndroidSaveDir() {
+    //access the underlying ANativeActivity structure from SFML
+    ANativeActivity* activity = sf::getNativeActivity();
+    if (activity != nullptr && activity->internalDataPath != nullptr) {
+        return std::string(activity->internalDataPath) + "/";
+    }else{
+    	std::cerr << "Failed to get internal directory path" << std::endl;
+    }
+    return "./";
+}
+#endif
+//++++++++++++++++++++++++++++++++++++
+
+
 int main(){
     sf::Font font = utils::load_font("font.ttf");
     sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "2048 game");
 
-
 //++++++++++++++++++++++++++++++++++++
-    #if defined(__ANDROID__)
-    #include <android/native_activity.h>
-    #include <SFML/System/NativeActivity.hpp>
-    
-    std::string getAndroidSaveDir() {
-        //access the underlying ANativeActivity structure from SFML
-        ANativeActivity* activity = sf::getNativeActivity();
-        if (activity != nullptr && activity->internalDataPath != nullptr) {
-            return std::string(activity->internalDataPath) + "/";
-        }else{
-        	std::cerr << "Failed to get internal directory path" << std::endl;
-        }
-        return "./";
-    }
-    #endif
-    
     #if defined(__ANDROID__)
     	SAVE_DIR = getAndroidSaveDir();
     	HIGH_SCORE_FILEPATH = SAVE_DIR + "high_scores.txt";
